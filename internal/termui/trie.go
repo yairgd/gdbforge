d5895df4ce3c51a87b911c9b19b782d323b03dd7
Yair Gadelov <yair.gadelov@gmail.com>
2026-06-21 14:04:12 +0300

    updfate trie.go

--- internal/termui/trie.go
+++ internal/termui/trie.go
@@ -44,74 +44,143 @@ func NewTrie() *Trie {
 	return &Trie{}
 
 }
+
+var keyMap = map[string]tcell.Key{
+	// Control
+	"C-a": tcell.KeyCtrlA,
+	"C-b": tcell.KeyCtrlB,
+	"C-c": tcell.KeyCtrlC,
+	"C-d": tcell.KeyCtrlD,
+	"C-e": tcell.KeyCtrlE,
+	"C-f": tcell.KeyCtrlF,
+	"C-g": tcell.KeyCtrlG,
+	"C-h": tcell.KeyCtrlH,
+	"C-i": tcell.KeyCtrlI,
+	"C-j": tcell.KeyCtrlJ,
+	"C-k": tcell.KeyCtrlK,
+	"C-l": tcell.KeyCtrlL,
+	"C-m": tcell.KeyCtrlM,
+	"C-n": tcell.KeyCtrlN,
+	"C-o": tcell.KeyCtrlO,
+	"C-p": tcell.KeyCtrlP,
+	"C-q": tcell.KeyCtrlQ,
+	"C-r": tcell.KeyCtrlR,
+	"C-s": tcell.KeyCtrlS,
+	"C-t": tcell.KeyCtrlT,
+	"C-u": tcell.KeyCtrlU,
+	"C-v": tcell.KeyCtrlV,
+	"C-w": tcell.KeyCtrlW,
+	"C-x": tcell.KeyCtrlX,
+	"C-y": tcell.KeyCtrlY,
+	"C-z": tcell.KeyCtrlZ,
+
+	// Navigation
+	"Up":    tcell.KeyUp,
+	"Down":  tcell.KeyDown,
+	"Left":  tcell.KeyLeft,
+	"Right": tcell.KeyRight,
+	"Home":  tcell.KeyHome,
+	"End":   tcell.KeyEnd,
+	"PgUp":  tcell.KeyPgUp,
+	"PgDn":  tcell.KeyPgDn,
+
+	// Editing
+	"Tab":       tcell.KeyTab,
+	"Enter":     tcell.KeyEnter,
+	"Esc":       tcell.KeyEscape,
+	"Backspace": tcell.KeyBackspace2,
+	"Delete":    tcell.KeyDelete,
+	"Insert":    tcell.KeyInsert,
+
+	// Function keys
+	"F1":  tcell.KeyF1,
+	"F2":  tcell.KeyF2,
+	"F3":  tcell.KeyF3,
+	"F4":  tcell.KeyF4,
+	"F5":  tcell.KeyF5,
+	"F6":  tcell.KeyF6,
+	"F7":  tcell.KeyF7,
+	"F8":  tcell.KeyF8,
+	"F9":  tcell.KeyF9,
+	"F10": tcell.KeyF10,
+	"F11": tcell.KeyF11,
+	"F12": tcell.KeyF12,
+}
+
+var keyToString = map[tcell.Key]string{
+	tcell.KeyCtrlA: "C-a",
+	tcell.KeyCtrlB: "C-b",
+	tcell.KeyCtrlC: "C-c",
+	tcell.KeyCtrlD: "C-d",
+	tcell.KeyCtrlE: "C-e",
+	tcell.KeyCtrlF: "C-f",
+	tcell.KeyCtrlG: "C-g",
+	tcell.KeyCtrlH: "C-h",
+	tcell.KeyCtrlI: "C-i",
+	tcell.KeyCtrlJ: "C-j",
+	tcell.KeyCtrlK: "C-k",
+	tcell.KeyCtrlL: "C-l",
+	tcell.KeyCtrlM: "C-m",
+	tcell.KeyCtrlN: "C-n",
+	tcell.KeyCtrlO: "C-o",
+	tcell.KeyCtrlP: "C-p",
+	tcell.KeyCtrlQ: "C-q",
+	tcell.KeyCtrlR: "C-r",
+	tcell.KeyCtrlS: "C-s",
+	tcell.KeyCtrlT: "C-t",
+	tcell.KeyCtrlU: "C-u",
+	tcell.KeyCtrlV: "C-v",
+	tcell.KeyCtrlW: "C-w",
+	tcell.KeyCtrlX: "C-x",
+	tcell.KeyCtrlY: "C-y",
+	tcell.KeyCtrlZ: "C-z",
+
+	tcell.KeyUp:    "Up",
+	tcell.KeyDown:  "Down",
+	tcell.KeyLeft:  "Left",
+	tcell.KeyRight: "Right",
+	tcell.KeyHome:  "Home",
+	tcell.KeyEnd:   "End",
+	tcell.KeyPgUp:  "PgUp",
+	tcell.KeyPgDn:  "PgDn",
+
+	tcell.KeyTab:        "Tab",
+	tcell.KeyEnter:      "Enter",
+	tcell.KeyEscape:     "Esc",
+	tcell.KeyBackspace2: "Backspace",
+	tcell.KeyDelete:     "Delete",
+	tcell.KeyInsert:     "Insert",
+
+	tcell.KeyF1:  "F1",
+	tcell.KeyF2:  "F2",
+	tcell.KeyF3:  "F3",
+	tcell.KeyF4:  "F4",
+	tcell.KeyF5:  "F5",
+	tcell.KeyF6:  "F6",
+	tcell.KeyF7:  "F7",
+	tcell.KeyF8:  "F8",
+	tcell.KeyF9:  "F9",
+	tcell.KeyF10: "F10",
+	tcell.KeyF11: "F11",
+	tcell.KeyF12: "F12",
+}
+
 func (t *Trie) parseToken(str string) Key {
-	var keyMap = map[string]tcell.Key{
-		// Control
-		"C-a": tcell.KeyCtrlA,
-		"C-b": tcell.KeyCtrlB,
-		"C-c": tcell.KeyCtrlC,
-		"C-d": tcell.KeyCtrlD,
-		"C-e": tcell.KeyCtrlE,
-		"C-f": tcell.KeyCtrlF,
-		"C-g": tcell.KeyCtrlG,
-		"C-h": tcell.KeyCtrlH,
-		"C-i": tcell.KeyCtrlI,
-		"C-j": tcell.KeyCtrlJ,
-		"C-k": tcell.KeyCtrlK,
-		"C-l": tcell.KeyCtrlL,
-		"C-m": tcell.KeyCtrlM,
-		"C-n": tcell.KeyCtrlN,
-		"C-o": tcell.KeyCtrlO,
-		"C-p": tcell.KeyCtrlP,
-		"C-q": tcell.KeyCtrlQ,
-		"C-r": tcell.KeyCtrlR,
-		"C-s": tcell.KeyCtrlS,
-		"C-t": tcell.KeyCtrlT,
-		"C-u": tcell.KeyCtrlU,
-		"C-v": tcell.KeyCtrlV,
-		"C-w": tcell.KeyCtrlW,
-		"C-x": tcell.KeyCtrlX,
-		"C-y": tcell.KeyCtrlY,
-		"C-z": tcell.KeyCtrlZ,
-
-		// Navigation
-		"Up":    tcell.KeyUp,
-		"Down":  tcell.KeyDown,
-		"Left":  tcell.KeyLeft,
-		"Right": tcell.KeyRight,
-		"Home":  tcell.KeyHome,
-		"End":   tcell.KeyEnd,
-		"PgUp":  tcell.KeyPgUp,
-		"PgDn":  tcell.KeyPgDn,
-
-		// Editing
-		"Tab":       tcell.KeyTab,
-		"Enter":     tcell.KeyEnter,
-		"Esc":       tcell.KeyEscape,
-		"Backspace": tcell.KeyBackspace2,
-		"Delete":    tcell.KeyDelete,
-		"Insert":    tcell.KeyInsert,
-
-		// Function keys
-		"F1":  tcell.KeyF1,
-		"F2":  tcell.KeyF2,
-		"F3":  tcell.KeyF3,
-		"F4":  tcell.KeyF4,
-		"F5":  tcell.KeyF5,
-		"F6":  tcell.KeyF6,
-		"F7":  tcell.KeyF7,
-		"F8":  tcell.KeyF8,
-		"F9":  tcell.KeyF9,
-		"F10": tcell.KeyF10,
-		"F11": tcell.KeyF11,
-		"F12": tcell.KeyF12,
-	}
 	return Key{
 		Key: keyMap[str],
 	}
 }
 
-func (t *Trie) Bind(str string, fn Callback) bool {
+func (t *Trie) keyToString(k Key) string {
+
+	if k.Key == tcell.KeyRune {
+		return string(k.Rune)
+	}
+
+	return "<" + keyToString[k.Key] + ">"
+}
+
+func (t *Trie) ParseSequence(str string) ([]Key, bool) {
 	var tmp string
 	var isCtl bool
 	var pending []Key
@@ -127,14 +196,25 @@ func (t *Trie) Bind(str string, fn Callback) bool {
 		} else if isCtl {
 			tmp += string(v)
 		} else {
-			pending = append(pending, Key{Rune: v})
+			pending = append(pending, Key{
+				Key:  tcell.KeyRune,
+				Rune: v,
+			})
 		}
 	}
 	if isCtl == true {
-		return false
+		return nil, false
 	} // error
-	t.insert(pending, fn)
-	return true
+	return pending, true
+}
+
+func (t *Trie) Bind(str string, fn Callback) bool {
+
+	if pending, ok := t.ParseSequence(str); ok {
+		t.insert(pending, fn)
+		return true
+	}
+	return false
 
 }
 
@@ -151,4 +231,30 @@ func (t *Trie) insert(pending []Key, onExactMatch Callback) {
 	}
 	root.IsTerminal = true
 	root.OnExactMatch = onExactMatch
+}
+
+func (t *Trie) Search(str string) bool {
+	root := &t.root
+
+	if pending, ok := t.ParseSequence(str); ok {
+		for _, key := range pending {
+			if child, ok := root.Children[key]; ok {
+				root = child
+			} else {
+				return false
+			}
+
+		}
+
+		if !root.IsTerminal {
+			return false
+		}
+
+		if root.OnExactMatch != nil {
+			root.OnExactMatch()
+		}
+
+		return true
+	}
+	return false
 }
