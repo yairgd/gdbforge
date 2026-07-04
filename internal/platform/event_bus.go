e794ac6423be3560d91c122f705b971b55ebb2ef
Yair Gadelov <yair.gadelov@gmail.com>
2026-07-04 19:07:04 +0300

    Adde devent bus

--- internal/platform/event_bus.go
+++ internal/platform/event_bus.go
@@ -1,23 +1,40 @@
 package platform
 
-type Event interface{}
-
-type Handler func(Event)
+import "sync"
 
 type EventBus struct {
-	handlers []Handler
+	mu sync.RWMutex
+
+	subs map[any][]func(any)
 }
 
 func NewEventBus() *EventBus {
-	return &EventBus{}
+	return &EventBus{
+		subs: make(map[any][]func(any)),
+	}
 }
 
-func (b *EventBus) Subscribe(h Handler) {
-	b.handlers = append(b.handlers, h)
+func Subscribe[T any](b *EventBus, h func(T)) {
+
+	var key *T
+
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.subs[key] = append(b.subs[key], func(v any) {
+		h(v.(T))
+	})
 }
 
-func (b *EventBus) Publish(ev Event) {
-	for _, h := range b.handlers {
+func Publish[T any](b *EventBus, ev T) {
+
+	var key *T
+
+	b.mu.RLock()
+	handlers := append([]func(any){}, b.subs[key]...)
+	b.mu.RUnlock()
+
+	for _, h := range handlers {
 		h(ev)
 	}
 }
